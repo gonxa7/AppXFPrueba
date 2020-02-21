098423c4c1610f19502028f8f7fb5bb372e16b84
gonxa7 <gonxa794@gmail.com>
2020-02-21 18:13:20 +0100

    Agregar archivos de proyecto.

---
+++ AppXFPrueba/AppXFPrueba/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppXFPrueba
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=0be3300f-eced-45f6-979b-8a6ef13d7436;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UITest1/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest1
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile("D:/AppXFPrueba/AppXFPrueba/AppXFPrueba.Android/bin/Release/com.companyname.appxfprueba.apk")
+                    .StartApp();
+                
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
